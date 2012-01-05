71f68d89b30928fdd392ead09a849f9f22d7a1a8
Ryan Hope <rmh3093@gmail.com>
2012-01-05 14:03:54 -0500

    update copyright info

--- src/plugin/util/point.cpp
+++ src/plugin/util/point.cpp
@@ -1,6 +1,7 @@
 /**
  * This file is part of SDLTerminal.
  * Copyright (C) 2011 Vincent Ho <www.whimsicalvee.com>
+ * Copyright (C) 2011-2012 Ryan Hope <rmh3093@gmail.com>
  *
  * SDLTerminal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
