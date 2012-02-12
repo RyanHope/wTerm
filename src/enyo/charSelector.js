8c1a9829fec664d8bf33369eeb13e1763b4104f0
Ryan Hope <rmh3093@gmail.com>
2012-02-12 13:16:33 -0500

    allow char selector to insert function keys

--- src/enyo/charSelector.js
+++ src/enyo/charSelector.js
@@ -9,6 +9,11 @@ enyo.kind({
 	align: 'center',
 	style: 'border-width: 1px; border-color: gray;',
 	
+	published: {
+		sym: null,
+		unicode: '',
+	},
+
 	downChanged: function() {
 		this.inherited(arguments);
 		if (this.down)
@@ -28,6 +33,15 @@ enyo.kind({
 		this.domStyles["border-bottom-style"] = bottom
 		this.domStyles["border-left-style"] = left
 		this.domStyles["border-right-style"] = right
+	},
+
+	setData: function(data) {
+		this.setUnicode(data[0])
+		this.setCaption(this.getUnicode())
+		if (data.length > 1) {
+			this.setSym(data[1])
+			this.applyStyle('color','#0000FF')
+		}
 	}
 
 })
@@ -42,33 +56,40 @@ enyo.kind({
 	width: '300px',
 	height: '300px',
 	
-	data: [ "$", "€", ";", "<", ">", "[", "\\", "]", "^", "`", "{", "|", "}", "~", "¡", "¢", "£", "¤", "¥", "§", "©", "ª", "«", "¬", "®", "°", "±", "º", "¹", "²", "³", "µ", "¶", "»", "¼", "½", "¾", "¿", "ƒ", "‘", "’", "‚", "‛", "“", "”", "„", "†", "‡", "•", "…", "‰", "‹", "›", "™", "Ω", "×", "÷", "Þ", "ß", "à", "á", "â", "ã", "ä", "å", "æ", "ç", "ć", "è", "é", "ê", "ë", "ę", "ē", "ì", "í", "î", "ï", "ł", "ð", "ñ", "ń", "ò", "ó", "ô", "õ", "ö", "ø", "ő", "œ", "š", "ù", "ú", "û", "ü", "ű", "ý", "ÿ", "ž", "ź", "ż", "þ", "À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "Ć", "È", "É", "Ê", "Ë", "Ę", "Ì", "Í", "Î", "Ï", "Ł", "Ð", "Ñ", "Ń", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "Ő", "Œ", "Š", "Ù", "Ú", "Û", "Ü", "Ű", "Ý", "Ÿ", "Ž", "Ź", "Ż", ":-(", ":-)", ";-)"],
+	chars: [
+		[ ["F1", SDLK._F1], ["F2", SDLK._F2], ["F3", SDLK._F3], ["F4", SDLK._F4], ["F5", SDLK._F5], ["F6", SDLK._F6], ["F7", SDLK._F7], ["F8", SDLK._F8], ["F9", SDLK._F9], ["F10", SDLK._F10], ["F11", SDLK._F11], ["F12", SDLK._F12]],
+		[ ["$"], ["€"], [";"], ["<"], [">"], ["["], ["\\"], ["]"], ["^"], ["`"], ["{"], ["|"], ["}"], ["~"], ["¡"], ["¢"], ["£"], ["¤"], ["¥"], ["§"], ["©"], ["ª"], ["«"], ["¬"], ["®"], ["°"], ["±"], ["º"], ["¹"], ["²"], ["³"], ["µ"], ["¶"], ["»"], ["¼"], ["½"], ["¾"], ["¿"], ["ƒ"], ["‘"], ["’"], ["‚"], ["‛"], ["“"], ["”"], ["„"], ["†"], ["‡"], ["•"], ["…"], ["‰"], ["‹"], ["›"], ["™"], ["Ω"], ["×"], ["÷"], ["Þ"], ["ß"], ["à"], ["á"], ["â"], ["ã"], ["ä"], ["å"], ["æ"], ["ç"], ["ć"], ["è"], ["é"], ["ê"], ["ë"], ["ę"], ["ē"], ["ì"], ["í"], ["î"], ["ï"], ["ł"], ["ð"], ["ñ"], ["ń"], ["ò"], ["ó"], ["ô"], ["õ"], ["ö"], ["ø"], ["ő"], ["œ"], ["š"], ["ù"], ["ú"], ["û"], ["ü"], ["ű"], ["ý"], ["ÿ"], ["ž"], ["ź"], ["ż"], ["þ"], ["À"], ["Á"], ["Â"], ["Ã"], ["Ä"], ["Å"], ["Æ"], ["Ç"], ["Ć"], ["È"], ["É"], ["Ê"], ["Ë"], ["Ę"], ["Ì"], ["Í"], ["Î"], ["Ï"], ["Ł"], ["Ð"], ["Ñ"], ["Ń"], ["Ò"], ["Ó"], ["Ô"], ["Õ"], ["Ö"], ["Ø"], ["Ő"], ["Œ"], ["Š"], ["Ù"], ["Ú"], ["Û"], ["Ü"], ["Ű"], ["Ý"], ["Ÿ"], ["Ž"], ["Ź"], ["Ż"] ],
+		[ ["="], ["\""], ["%"], ["_"], ["$"], ["€"], [";"], ["<"], [">"], ["["], ["\\"], ["]"], ["^"], ["`"], ["{"], ["|"], ["}"], ["~"], ["¡"], ["¢"], ["£"], ["¤"], ["¥"], ["§"], ["©"], ["ª"], ["«"], ["¬"], ["®"], ["°"], ["±"], ["º"], ["¹"], ["²"], ["³"], ["µ"], ["¶"], ["»"], ["¼"], ["½"], ["¾"], ["¿"], ["ƒ"], ["‘"], ["’"], ["‚"], ["‛"], ["“"], ["”"], ["„"], ["†"], ["‡"], ["•"], ["…"], ["‰"], ["‹"], ["›"], ["™"], ["Ω"], ["×"], ["÷"], ["Þ"], ["ß"], ["à"], ["á"], ["â"], ["ã"], ["ä"], ["å"], ["æ"], ["ç"], ["ć"], ["è"], ["é"], ["ê"], ["ë"], ["ę"], ["ē"], ["ì"], ["í"], ["î"], ["ï"], ["ł"], ["ð"], ["ñ"], ["ń"], ["ò"], ["ó"], ["ô"], ["õ"], ["ö"], ["ø"], ["ő"], ["œ"], ["š"], ["ù"], ["ú"], ["û"], ["ü"], ["ű"], ["ý"], ["ÿ"], ["ž"], ["ź"], ["ż"], ["þ"], ["À"], ["Á"], ["Â"], ["Ã"], ["Ä"], ["Å"], ["Æ"], ["Ç"], ["Ć"], ["È"], ["É"], ["Ê"], ["Ë"], ["Ę"], ["Ì"], ["Í"], ["Î"], ["Ï"], ["Ł"], ["Ð"], ["Ñ"], ["Ń"], ["Ò"], ["Ó"], ["Ô"], ["Õ"], ["Ö"], ["Ø"], ["Ő"], ["Œ"], ["Š"], ["Ù"], ["Ú"], ["Û"], ["Ü"], ["Ű"], ["Ý"], ["Ÿ"], ["Ž"], ["Ź"], ["Ż"] ],
+	],
 	
 	published: {
 		terminal: null
 	},
 
 	components: [
-  	{kind: "VirtualList", width: '300px', height: '300px', style: 'padding: 0px; margin: 0px', onSetupRow: "setupRow", components: [
-    	{kind: "Item", width: '300px', style: 'padding: 0px; margin: 0px', name: 'charRow', layoutKind: 'HFlexLayout', components: [
-      	{kind: "charKey", name: 'b1', onmouseup: 'doMouseup'},
-        {kind: "charKey", name: 'b2', onmouseup: 'doMouseup'},
-        {kind: "charKey", name: 'b3', onmouseup: 'doMouseup'},
-        {kind: "charKey", name: 'b4', onmouseup: 'doMouseup'},
-        {kind: "charKey", name: 'b5', onmouseup: 'doMouseup'},
+		{kind: "VirtualList", width: '300px', height: '300px', style: 'padding: 0px; margin: 0px', onSetupRow: "setupRow", components: [
+			{kind: "Item", width: '300px', style: 'padding: 0px; margin: 0px', name: 'charRow', layoutKind: 'HFlexLayout', components: [
+				{kind: "charKey", name: 'b1', onmouseup: 'doMouseup'},
+				{kind: "charKey", name: 'b2', onmouseup: 'doMouseup'},
+				{kind: "charKey", name: 'b3', onmouseup: 'doMouseup'},
+				{kind: "charKey", name: 'b4', onmouseup: 'doMouseup'},
+				{kind: "charKey", name: 'b5', onmouseup: 'doMouseup'},
 			]}
 		]}
 	],
 	
 	doMouseup: function(inSender, inEvent) {
-		//this.log(inSender)
-		this.terminal.inject(inSender.getCaption(), 1)
+		this.terminal.keyDown(0, inSender.sym, inSender.unicode, 1);
+		this.terminal.keyUp(0, inSender.sym, inSender.unicode, 1);
 		this.close()
 	},
 	
 	create: function() {
 		this.inherited(arguments);
-		//this.log(this.className)
+		if (enyo.g11n.currentLocale().getLanguage() == 'en')
+			this.data = this.chars[0].concat(this.chars[1])
+		else
+			this.data = this.chars[0].concat(this.chars[2])
 	},
 	
 	setupRow: function(inSender, inIndex) {
@@ -87,23 +108,23 @@ enyo.kind({
 			this.$.b5.setBorder(borderTop, "solid", "none", "none")
 			var i = inIndex * 5
 			if (i+0 < this.data.length)
-				this.$.b1.setCaption(this.data[i+0])
+				this.$.b1.setData(this.data[i+0])
 			else
 				this.$.b1.setShowing(false)
 			if (i+1 < this.data.length)
-				this.$.b2.setCaption(this.data[i+1])
+				this.$.b2.setData(this.data[i+1])
 			else
 				this.$.b2.setShowing(false)
 			if (i+2 < this.data.length)
-				this.$.b3.setCaption(this.data[i+2])
+				this.$.b3.setData(this.data[i+2])
 			else
 				this.$.b3.setShowing(false)
 			if (i+3 < this.data.length)
-				this.$.b4.setCaption(this.data[i+3])
+				this.$.b4.setData(this.data[i+3])
 			else
 				this.$.b4.setShowing(false)
 			if (i+4 < this.data.length)
-				this.$.b5.setCaption(this.data[i+4])
+				this.$.b5.setData(this.data[i+4])
 			else
 				this.$.b5.setShowing(false)
 			return true;
